cd5c5035b4c05e7a786e6790de721b663616cd76
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2023-11-20 17:09:07 +0100

    add home

---
+++ nutri_app/lib/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    //ExploreScreen(),
+    //RecipesScreen(),
+    //GroceryScreen(),
+  ];
+  void onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //return Consumer<TabManager>(
+      //builder: (context, tabManager, child){
+        return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        body: pages[_selectedIndex], //tabManager.selectedTab
+        bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              label: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Recipes',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'To Buy',
+            ),
+          ],
+        currentIndex: _selectedIndex, //tabManager.selectedTab
+        onTap: onItemTapped,
+        ),
+        );
+      }
+}
