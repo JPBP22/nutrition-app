412a86b5f738bb023ba900e6e4913a97d25ba39f
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2023-12-09 23:18:27 +0100

    Create run_thread_service_json.dart

---
+++ nutri_app/lib/service/run_thread_service_json.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+import '../secrets.dart';
+
+class RunThreadService {
+  final String _apiKey = Secrets.API_KEY;
+  final String _assistantId = Secrets.ASSISTANT_JSON_ID;
+
+  Future<http.Response> runThread(String threadId) async {
+    var url = Uri.parse('https://api.openai.com/v1/threads/$threadId/runs');
+    try {
+      var response = await http.post(url,
+          headers: {
+            'Authorization': 'Bearer $_apiKey',
+            'Content-Type': 'application/json',
+            'OpenAI-Beta': 'assistants=v1'
+          },
+          body: jsonEncode({
+            'assistant_id': _assistantId,
+          }));
+      print('RunThreadService Response: ${response.body}');
+      return response;
+    } catch (e) {
+      print('Error in runThread: $e');
+      rethrow;
+    }
+  }
+}
