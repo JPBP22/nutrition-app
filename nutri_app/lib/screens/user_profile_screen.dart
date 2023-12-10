fcbd1aabb30b88d6c9b7b9bae31199e48b633779
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2023-12-10 17:52:14 +0100

    finished user login

--- nutri_app/lib/screens/user_profile_screen.dart
+++ nutri_app/lib/screens/user_profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';  
@@ -8,10 +7,11 @@ import '../models/models.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../auth.dart';
 import 'package:provider/provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:go_router/go_router.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
-
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
@@ -21,25 +21,105 @@ class _ProfileScreenState extends State<ProfileScreen> {
   double screenWidth = 0;
   Color primary = const Color(0xffeef444c);
   String profilePicLink = "";
+  File? _selectedImage;
 
-  Future pickUploadProfilePic() async {
+  late Future<String> _userPhotoFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _userPhotoFuture = getUserPhoto();
+  }
+
+  Future<String> pickUploadProfilePic() async {
+    await requestCameraPermission();
     final image = await ImagePicker().pickImage(
-      source: ImageSource.camera,
+      source: ImageSource.gallery,
       maxHeight: 512,
       maxWidth: 512,
       imageQuality: 90,
     );
 
-    Reference ref = FirebaseStorage.instance
-        .ref().child("profilepic.jpg");
-
-    await ref.putFile(File(image!.path));
-
-    ref.getDownloadURL().then((value) async {
+    if (image != null) {
       setState(() {
-       profilePicLink = value;
+        _selectedImage = File(image.path); // Update _selectedImage here
       });
-    });
+
+      String userId = FirebaseAuth.instance.currentUser!.uid;
+      CollectionReference media = FirebaseFirestore.instance.collection('users').doc(userId).collection('media');
+
+      if (_selectedImage != null) {
+        // Upload image file to Firebase Storage
+        var imageName = DateTime.now().millisecondsSinceEpoch.toString();
+        var storageRef = FirebaseStorage.instance.ref().child('profile_images/$userId/$imageName.jpg');
+        var uploadTask = storageRef.putFile(_selectedImage!);
+        var downloadUrl = await uploadTask.then((snapshot) => snapshot.ref.getDownloadURL());
+
+        // Delete the old image from Firebase Storage
+        if (profilePicLink.isNotEmpty) {
+          var oldImageRef = FirebaseStorage.instance.refFromURL(profilePicLink);
+          await oldImageRef.delete();
+        }
+        // Update the document with the new image reference
+        var userDoc = await media.doc(userId).get();
+        if (userDoc.exists) {
+          await userDoc.reference.update({
+            'profile_pic_url': downloadUrl.toString(),
+          });
+        } else {
+          await media.doc(userId).set({
+            'profile_pic_url': downloadUrl.toString(),
+          });
+        }
+
+        // Update profilePicLink with the new URL
+        setState(() {
+          profilePicLink = downloadUrl.toString();
+        });
+
+        print(downloadUrl.toString());
+
+        return downloadUrl.toString();
+      } else {
+        throw Exception('No image selected');
+      }
+    }
+    return "";  
+  }
+
+  Future<String> getUserPhoto() async {
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    QuerySnapshot mediaSnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .collection('media')
+        .get();
+
+    if (mediaSnapshot.docs.isNotEmpty) {
+      var mediaData = mediaSnapshot.docs.first.data() as Map<String, dynamic>;
+      if (mediaData['profile_pic_url'] != null) {
+        print('Got profile_pic_url'); 
+        if (mounted) {
+        setState(() {
+          profilePicLink = mediaData['profile_pic_url'];
+        });
+        }
+      }
+    }
+  }
+  return profilePicLink;
+}
+    
+  Future<void> requestCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      status = await Permission.camera.request();
+    }
+
+    if (status.isDenied) {
+      print('Camera permission was denied');
+    }
   }
 
   Future<DocumentSnapshot> getUserData() async {
@@ -57,14 +137,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Widget _signOutButton(BuildContext context){
     return ElevatedButton.icon(
-    icon: Icon(Icons.logout),
-    label: Text('Logout'),
+    icon: const Icon(Icons.logout),
+    label: const Text('Logout'),
     onPressed: () async {
-      if (context != null) {
-        Provider.of<AppStateManager>(context, listen: false).logout();
-        await signOut();
-        }
-      },
+      Provider.of<AppStateManager>(context, listen: false).logout();
+      await signOut();
+            },
       style: ElevatedButton.styleFrom(
         primary: Colors.green, // background color
         onPrimary: Colors.white, // text color
@@ -83,38 +161,76 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: SingleChildScrollView(
         child: Container(
           alignment: Alignment.topCenter,
-            padding: const EdgeInsets.all(20),
+            padding: const EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start, // Center children horizontally
               children: [
-                GestureDetector(
-                  onTap: () {
-                    pickUploadProfilePic();
+                Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    context.go('/home');
                   },
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 40, left: 40, right: 40, bottom: 20),
-                    height: 120,
-                    width: 120,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.green,
-                    ),
-                    child: profilePicLink.isEmpty ? const Icon(
-                        Icons.person,
-                        color: Colors.white,
-                        size: 80,
-                      ) : ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(profilePicLink),
-                      ),
-                    ),
-                  ),
+                ),
+              ],
+            ),
+                GestureDetector(
+                onTap: () async {
+                  pickUploadProfilePic(); // Call pickUploadProfilePic when the GestureDetector is tapped
+                },
+                child: FutureBuilder<String>(
+                  future: _userPhotoFuture,
+                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator(); // Show a loading spinner while waiting for the future to complete
+                    } else if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return const Icon(Icons.error); // Show an error icon if the future completes with an error
+                    } else {
+                      return (snapshot.data == null || snapshot.data!.isEmpty)
+                    ? Container(
+                        margin: const EdgeInsets.only(top: 5, left: 40, right: 40, bottom: 20),
+                        height: 120,
+                        width: 120,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.green,
+                        ),
+                        child: const Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                      )
+                    : Padding(
+                      padding: const EdgeInsets.only(top: 5, left: 40, right: 40, bottom: 20),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            profilePicLink,
+                            height: 120,
+                            width: 120,
+                            fit: BoxFit.fill,
+                            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                              print('Failed to load image: $exception');
+                              print('Loading image from: $profilePicLink');
+                              return const Text('Failed to load image');
+                            },
+                          ),
+                        ),
+                    );
+                    }
+                    }
+                ),
+              ),
                 FutureBuilder<DocumentSnapshot>(
                   future: getUserData(),
                   builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
+                      return const CircularProgressIndicator();
                     } else if (snapshot.hasError) {
                       // Handle the error
                       return Text('Error: ${snapshot.error}');
@@ -137,7 +253,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       );
                     } else {
                       // Handle the case when snapshot doesn't have data
-                      return Text('No data');
+                      return const Text('No data');
                     }
                   },
                 ),
@@ -148,4 +264,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
-  }
+}
